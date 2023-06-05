3607dc4b23d25333b9d37a211c2b1d4f46ebb86f
conan-exploits <matvijivanina@gmail.com>
2023-06-05 23:52:30 +0300

    зміна функціоналу програми шляхом переміщення функціоналу в Exercise

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,65 @@
 package domain;
 
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+    public static void sort(int[] arr) {
+        int[] temp = new int[arr.length];
+        int blockSize = 1;
+        while (blockSize < arr.length) {
+            mergePass(arr, temp, blockSize);
+            blockSize *= 2;
+            mergePass(temp, arr, blockSize);
+            blockSize *= 2;
+        }
+    }
+
+    public static void mergePass(int[] input, int[] output, int blockSize) {
+        int n = input.length;
+        int i = 0;
+        while (i < n) {
+            merge(input, output, i, blockSize);
+            i += blockSize * 2;
+        }
+        for (i = 0; i < n; i++) {
+            input[i] = output[i];
+        }
+    }
+
+    public static void merge(int[] input, int[] output, int start, int blockSize) {
+        int i = start;
+        int mid = Math.min(start + blockSize, input.length);
+        int j = mid;
+        int end = Math.min(start + blockSize * 2, input.length);
+        int k = start;
+
+        while (i < mid && j < end) {
+            if (input[i] < input[j]) {
+                output[k++] = input[i++];
+            } else if (input[i] > input[j]) {
+                output[k++] = input[j++];
+            } else {
+                i++;
+            }
+        }
+
+        while (i < mid) {
+            output[k++] = input[i++];
+        }
+
+        while (j < end) {
+            output[k++] = input[j++];
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {9, 4, 2, 7, 5, 1, 8, 3, 6};
+        sort(arr);
+        printArray(arr);
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,33 +1,18 @@
 package test;
 
-//import domain.Exercise;
-import java.util.*;
+import domain.Exercise;
 
 public class TestResult {
-
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 4, 4, 5, 5, 6, 6, 6, 6}; // змініть масив за своїм бажанням
-        int mostFrequent = findMostFrequent(arr);
-        System.out.println("Number the most repeats: " + mostFrequent);
+        int[] arr = {9, 4, 2, 7, 5, 1, 8, 3, 6};
+        Exercise.sort(arr);
+        printArray(arr);
     }
-    public static int findMostFrequent(int[] arr) {
-    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-    int mostFrequent = arr[0];
-    int maxCount = 1;
-    for (int i = 0; i < arr.length; i++) {
-      int num = arr[i];
-      if (map.containsKey(num)) {
-        int count = map.get(num) + 1;
-        map.put(num, count);
-        if (count > maxCount) {
-          maxCount = count;
-          mostFrequent = num;
-        }
-      } else {
-        map.put(num, 1);
-      }
-    }
-    return mostFrequent;
-  }
-}
 
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
